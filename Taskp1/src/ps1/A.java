f312857b42843364ebcf93f272a66cd92d7de744
RESHU_SYED <RESHU_SYED@192.168.20.25>
2019-05-12 02:49:55 -0700

    Added push

---
+++ Taskp1/src/ps1/A.java
@@ -0,0 +1,18 @@
+package ps1;
+
+public class A {
+
+	float x = 10;
+	boolean y = false;
+
+	public void m1() {
+		System.out.println(x);
+
+	}
+
+	public static void main(String[] args) {
+		A obj = new A();
+		obj.m1();
+	}
+
+}
